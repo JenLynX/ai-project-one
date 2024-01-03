c87371b151e97733d9de71d8c48d80bf2ddc306d
JenLynX <104281812+JenLynX@users.noreply.github.com>
2024-01-03 15:30:39 -0800

    added javascript and typewriter plug in

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Poems here. Poems there. Poems are everywhere.",
+    autoStart: true,
+    delay: 39,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
